899888eb0f07186e2356db8bb55abaa60a1a4393
AmoghASDRP <amogh.chaudhary@students.asdrp.org>
2026-07-03 03:24:49 +0000

    Jul 2, 2026, 8:24 PM
    Updated play function

--- main.py
+++ main.py
@@ -7,11 +7,52 @@ def main():
 # must also be recursive
 # Amogh is doing this one
 def play():
-    lives = 6
     word = chooseWord()
-    word_list = []
+    # for word progress bar
     progress = []
+    used = []
+    lives = 6
+    for letter in word:
+        progress.append("_")
+    while lives > 0 and "_" in progress:
+        # display current progress & lives
+        print(" ".join(progress)) 
+        print("Lives left:", lives)
+        guess = input("Guess a letter or guess answer: ").lower().strip()
+
+        if guess == "help":
+            displayUsedLetters(used)
+            continue
+
+        if guess == "guess answer":
+            guess = input("Enter the full word: ")
+            
+            if guess == word.lower().strip():
+                print("Word guessed, you've won!")
+                break
+            else:
+                print("Answer wrong, game over!")
+                break
+            
+            
+        else:
+            isCorrect = guessLetter(guess, word, progress, used)
+            if not isCorrect:
+                print("Letter not found!")
+                lives -= 1
+                
+    if "_" not in progress:
+        print("Word guessed, you've won!")
+
+    else:
+        print("Out of lives, game over!")
     
+    replay = input("Play again? (yes/no)").lower().strip()
+    while replay != "yes" and replay != "no": 
+        replay = input("Invalid input. Play again? (yes/no)").lower().strip()
+    if replay == "yes":
+        print("New game!")
+        play()
 
 
 
@@ -22,16 +63,20 @@ def chooseWord():
     pass
 
 # guessLetter: validates input and check if the letter guessed is valid; if so, 
-# checks whether it's in the word
-# should probably return true or false
-def guessLetter():
+# must be a single alphabetic character
+# recursively reprompts in case of user giving invalid input
+# if valid, checks if letter is in word
+# updates progress and used variable
+# returns True if correct, False otherwise
+
+def guessLetter(guess, word, progress, used):
     pass
 
-# guessWord: checks whether the word guessed is the word
-# should probably return true or false
-def guessWord():
-    pass
 
+# displayUsedLetters: displays the letters used in the correct format as per the project 1 PDF
+# bonus function
+def displayUsedLetters(used):
+    pass
 
 if __name__ == "__main__":
     main()
